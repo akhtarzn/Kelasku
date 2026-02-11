5be8b18ee1b191e45ed890abd38e7c273efc4aa5
akhtarzn <akhtarzn@outlook.com>
2026-02-11 09:25:01 +0700

    Update lesson_schedule.js

--- lesson_schedule.js
+++ lesson_schedule.js
@@ -43,7 +43,8 @@ const teacherMap = {
   "e49a2308-41e6-4a75-8a69-ad4af33b27de": "Dra. Mariah Ulfah",
   "d8ab8eaf-f4c3-4beb-81e6-3913a7352f91": "Dinda Triana,
   "7988a7f6-f758-41e2-8874-1cf7d9bd3be7": "Dhela",
-  "f4f0e3e2-259b-4c20-a29d-d662dee85cb5": "Lastri"
+  "f4f0e3e2-259b-4c20-a29d-d662dee85cb5": "Lastri",
+  "ffa7ce13-5968-4b85-b534-b5e17c1304bd": "Miftahul Jannah, S.Sn"
 };
 
 function formatTanggal(isoString) {
